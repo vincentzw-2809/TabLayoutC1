08cef7da792d433ea5adaf94e3074d129950e076
panggau <92567925+vincentzw-2809@users.noreply.github.com>
2023-05-08 11:07:07 +0700

    vincentzw-202102278

---
+++ app/src/main/java/com/vincentzw202102278/tablayoutc1/MyFragmentPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.vincentzw202102278.tablayoutc1;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter
+{
+
+    private BeritaFragment _beritaFragment = new BeritaFragment();
+    private Context _context;
+    private ECommerceFragment _ecommerceFragment = new ECommerceFragment();
+    private int _tabCount;
+    private KampusFragment _kampusFragment = new KampusFragment();
+
+    public MyFragmentPagerAdapter (Context context, FragmentManager fragmentManager, int tabCount)
+    {
+        super(fragmentManager);
+
+        _context = context;
+        _tabCount = tabCount;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position)
+    {
+        switch (position)
+        {
+            case 0:
+                return _ecommerceFragment;
+            case 1:
+                return _beritaFragment;
+            default:
+                return _kampusFragment;
+        }
+    }
+
+    @Override
+    public int getCount()
+    {
+        return _tabCount;
+    }
+}
